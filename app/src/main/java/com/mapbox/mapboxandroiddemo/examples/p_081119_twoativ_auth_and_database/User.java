e96d109f513e0f18b5414d908878eae985f007fc
Alex22485 <alex.224.85@mail.ru>
2019-11-17 12:58:24 +0700

    добавлена инфа в database как в примере

---
+++ app/src/main/java/com/mapbox/mapboxandroiddemo/examples/p_081119_twoativ_auth_and_database/User.java
@@ -0,0 +1,64 @@
+package com.mapbox.mapboxandroiddemo.examples.p_081119_twoativ_auth_and_database;
+
+public class User {
+    private String Name;
+    private String Email;
+    private String Phone;
+    private String UserName;
+    private String Password;
+
+    public User() {
+    }
+
+    public User(String name, String email, String phone, String userName, String password) {
+        Name = name;
+        Email = email;
+        Phone = phone;
+        UserName = userName;
+        Password = password;
+
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String phone) {
+        Phone = phone;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+
+}
+
