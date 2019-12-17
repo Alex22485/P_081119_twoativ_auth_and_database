4aa83ac301d3881d1a5079b519ec1c7027e59926
Alex Glushkov@ <alex.224.85@mail.ru>
2019-12-18 00:51:01 +0700

    первое уведомление пробное делал по видео. работает, но нихрена не понятно

---
+++ app/src/main/java/com/mapbox/mapboxandroiddemo/examples/p_081119_twoativ_auth_and_database/GettingDeviceTokenService.java
@@ -0,0 +1,20 @@
+package com.mapbox.mapboxandroiddemo.examples.p_081119_twoativ_auth_and_database;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdReceiver;
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class GettingDeviceTokenService extends FirebaseMessagingService {
+
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+        Log.d("NEW_TOKEN",s);
+    }
+
+}
