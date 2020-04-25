bccb7bbce9a510e917a78d6a84a0cd2a79758acc
Alex22485 <alex.224.85@mail.ru>
2020-04-25 16:59:47 +0700

    модернизация продолжается ServApp DriverApp1

--- app/src/main/java/com/mapbox/mapboxandroiddemo/examples/p_081119_twoativ_auth_and_database/DriversApp_1.java
+++ app/src/main/java/com/mapbox/mapboxandroiddemo/examples/p_081119_twoativ_auth_and_database/DriversApp_1.java
@@ -5,7 +5,9 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.database.ChildEventListener;
@@ -23,7 +25,7 @@ public class DriversApp_1 extends AppCompatActivity {
 
     TextView DataOrder;
     TextView TimeOrder;
-    TextView MapOrder;
+    String MapOrder;
     TextView RoutepOrder;
     TextView StopOrder1;
     TextView MenpOrder1;
@@ -33,6 +35,26 @@ public class DriversApp_1 extends AppCompatActivity {
     TextView MenpOrder3;
     TextView StopOrder4;
     TextView MenpOrder4;
+
+    TextView TextStopOder1;
+    TextView TextStopOder2;
+    TextView TextStopOder3;
+    TextView TextStopOder4;
+
+    TextView TextMenpOrder1;
+    TextView TextMenpOrder2;
+    TextView TextMenpOrder3;
+    TextView TextMenpOrder4;
+    TextView TextRoutepOrder;
+    //TextView TextMapOrder;
+
+    TextView TextDataOrder;
+    TextView TextTimeOrder;
+
+    Button BtnYes;
+    Button BtnNo;
+    Button timeExpl;
+    Button MapSee;
 
     FirebaseDatabase database01;
     DatabaseReference ref01;
@@ -51,9 +73,12 @@ public class DriversApp_1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drivers_app_1);
 
+        TextDataOrder=findViewById(R.id.TextDataOrder);
+        TextTimeOrder=findViewById(R.id.TextTimeOrder);
+
         DataOrder=findViewById(R.id.DataOrder);
         TimeOrder=findViewById(R.id.TimeOrder);
-        MapOrder=findViewById(R.id.MapOrder);
+        //MapOrder=findViewById(R.id.MapOrder);
         RoutepOrder=findViewById(R.id.RoutepOrder);
         StopOrder1=findViewById(R.id.StopOrder1);
         MenpOrder1=findViewById(R.id.MenpOrder1);
@@ -63,6 +88,26 @@ public class DriversApp_1 extends AppCompatActivity {
         MenpOrder3=findViewById(R.id.MenpOrder3);
         StopOrder4=findViewById(R.id.StopOrder4);
         MenpOrder4=findViewById(R.id.MenpOrder4);
+
+        TextStopOder1=findViewById(R.id.TextStopOder1);
+        TextStopOder2=findViewById(R.id.TextStopOder2);
+        TextStopOder3=findViewById(R.id.TextStopOder3);
+        TextStopOder4=findViewById(R.id.TextStopOder4);
+
+        TextMenpOrder1=findViewById(R.id.TextMenpOrder1);
+        TextMenpOrder2=findViewById(R.id.TextMenpOrder2);
+        TextMenpOrder3=findViewById(R.id.TextMenpOrder3);
+        TextMenpOrder4=findViewById(R.id.TextMenpOrder4);
+        TextRoutepOrder=findViewById(R.id.TextRoutepOrder);
+        //TextMapOrder=findViewById(R.id.TextMapOrder);
+
+
+
+
+        BtnYes=findViewById(R.id.BtnYes);
+        BtnNo=findViewById(R.id.BtnNo);
+        timeExpl=findViewById(R.id.timeExpl);
+        MapSee=findViewById(R.id.MapSee);
 
 
     }
@@ -76,7 +121,7 @@ public class DriversApp_1 extends AppCompatActivity {
             public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
                 String time=dataSnapshot.child( "рейс" ).getValue(String.class);
                 String date=dataSnapshot.child( "дата" ).getValue(String.class);
-                String направление=dataSnapshot.child( "направление" ).getValue(String.class);
+                MapOrder=dataSnapshot.child( "направление" ).getValue(String.class);
                 String маршрут=dataSnapshot.child( "маршрут" ).getValue(String.class);
                 String point1=dataSnapshot.child( "точкаСбора1" ).getValue(String.class);
                 String point1Men=dataSnapshot.child( "точкаСбора1Чел" ).getValue(String.class);
@@ -89,7 +134,7 @@ public class DriversApp_1 extends AppCompatActivity {
 
                 DataOrder.setText(date);
                 TimeOrder.setText(time);
-                MapOrder.setText(направление);
+                //MapOrder.setText(направление);
                 RoutepOrder.setText(маршрут);
                 StopOrder1.setText(point1);
                 MenpOrder1.setText(point1Men);
@@ -99,6 +144,8 @@ public class DriversApp_1 extends AppCompatActivity {
                 MenpOrder3.setText(point3Men);
                 StopOrder4.setText(point4);
                 MenpOrder4.setText(point4Men);
+
+                visability ();
             }
             @Override
             public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
@@ -115,6 +162,90 @@ public class DriversApp_1 extends AppCompatActivity {
         });
     }
 
+    public void visability (){
+
+        String onePoint=StopOrder1.getText().toString();
+        String twoPoint=StopOrder2.getText().toString();
+        String treePoint=StopOrder3.getText().toString();
+        String fourPoint=StopOrder4.getText().toString();
+        Log.d("TAG", "вторая точка" + twoPoint);
+
+
+        if(!onePoint.isEmpty()){
+            StopOrder1.setVisibility(View.VISIBLE);
+            StopOrder1.setTextColor(getResources().getColor( R.color.colorNew ));
+            TextStopOder1.setVisibility(View.VISIBLE);
+            TextMenpOrder1.setVisibility(View.VISIBLE);
+            MenpOrder1.setVisibility(View.VISIBLE);
+            MenpOrder1.setTextColor(getResources().getColor( R.color.colorNew ));
+
+            //Видимость общего текста
+            visabileGeneralText();
+
+    }
+        if (!twoPoint.isEmpty()){
+            StopOrder2.setVisibility(View.VISIBLE);
+            StopOrder2.setTextColor(getResources().getColor( R.color.colorNew ));
+            TextStopOder2.setVisibility(View.VISIBLE);
+            TextMenpOrder2.setVisibility(View.VISIBLE);
+            MenpOrder2.setVisibility(View.VISIBLE);
+            MenpOrder2.setTextColor(getResources().getColor( R.color.colorNew ));
+
+            //Видимость общего текста
+            visabileGeneralText();
+        }
+        if (!treePoint.isEmpty()){
+            StopOrder3.setVisibility(View.VISIBLE);
+            StopOrder3.setTextColor(getResources().getColor( R.color.colorNew ));
+            TextStopOder3.setVisibility(View.VISIBLE);
+            TextMenpOrder3.setVisibility(View.VISIBLE);
+            MenpOrder3.setVisibility(View.VISIBLE);
+            MenpOrder3.setTextColor(getResources().getColor( R.color.colorNew ));
+
+            //Видимость общего текста
+            visabileGeneralText();
+        }
+        if (!fourPoint.isEmpty()){
+            StopOrder4.setVisibility(View.VISIBLE);
+            StopOrder4.setTextColor(getResources().getColor( R.color.colorNew ));
+            TextStopOder4.setVisibility(View.VISIBLE);
+            TextMenpOrder4.setVisibility(View.VISIBLE);
+            MenpOrder4.setVisibility(View.VISIBLE);
+            MenpOrder4.setTextColor(getResources().getColor( R.color.colorNew ));
+
+            //Видимость общего текста
+            visabileGeneralText();
+        }
+
+    }
+    //Видимость общего текста
+    public void visabileGeneralText(){
+
+        BtnYes.setVisibility(View.VISIBLE);
+        BtnNo.setVisibility(View.VISIBLE);
+        MapSee.setVisibility(View.VISIBLE);
+
+        DataOrder.setVisibility(View.VISIBLE);
+        DataOrder.setTextColor(getResources().getColor( R.color.colorNew ));
+
+//        MapOrder.setVisibility(View.VISIBLE);
+//        MapOrder.setTextColor(getResources().getColor( R.color.colorNew ));
+
+        RoutepOrder.setVisibility(View.VISIBLE);
+        RoutepOrder.setTextColor(getResources().getColor( R.color.colorNew ));
+
+        TextDataOrder.setVisibility(View.VISIBLE);
+        TextTimeOrder.setVisibility(View.VISIBLE);
+        TextRoutepOrder.setVisibility(View.VISIBLE);
+//        TextMapOrder.setVisibility(View.VISIBLE);
+
+        TimeOrder.setVisibility(View.VISIBLE);
+        TimeOrder.setTextColor(getResources().getColor( R.color.colorNew ));
+
+        timeExpl.setVisibility(View.VISIBLE);
+
+    }
+
     //Принять заявку 26 03 2020 c записью в БД в нужную ветку заявки
     // нужно изменить путь child( "123Lexus" ) для любого водителя чтобы работал Н-р id
     public void BtnYes (View view){
@@ -126,15 +257,15 @@ public class DriversApp_1 extends AppCompatActivity {
         // если строка пусатя то запись в БД не будет
         String abcdRef =  "";
 
-        if (a.equals(abcdRef)){}
-        else {
+        if (!a.isEmpty()){
+
             //Получить текущую дату
             getTimNow();
 
             //260320 Запись в БД Заявки--...--Users-Заявки водителя принявшего заявку
             database01 = FirebaseDatabase.getInstance();
             ref01 = database01.getReference("Заявки")
-                    .child(MapOrder.getText().toString())
+                    .child(MapOrder)
                     .child(DataOrder.getText().toString())
                     .child(TimeOrder.getText().toString())
                     .child(RoutepOrder.getText().toString())
@@ -155,14 +286,14 @@ public class DriversApp_1 extends AppCompatActivity {
         }
 
         // запись пустой точки номер два ПРОБА
-        if (b.equals(abcdRef)){}
-        else {
+        if (!b.isEmpty()){
+
             //Получить текущую дату
             getTimNow();
 
             database02 = FirebaseDatabase.getInstance();
             ref02 = database02.getReference("Заявки")
-                    .child(MapOrder.getText().toString())
+                    .child(MapOrder)
                     .child(DataOrder.getText().toString())
                     .child(TimeOrder.getText().toString())
                     .child(RoutepOrder.getText().toString())
@@ -183,15 +314,14 @@ public class DriversApp_1 extends AppCompatActivity {
         }
 
         // запись пустой точки номер три ПРОБА
-        if (c.equals(abcdRef)){}
-        else {
+        if (!c.isEmpty()){
 
             //Получить текущую дату
             getTimNow();
 
             database03 = FirebaseDatabase.getInstance();
             ref03 = database03.getReference("Заявки")
-                    .child(MapOrder.getText().toString())
+                    .child(MapOrder)
                     .child(DataOrder.getText().toString())
                     .child(TimeOrder.getText().toString())
                     .child(RoutepOrder.getText().toString())
@@ -212,14 +342,13 @@ public class DriversApp_1 extends AppCompatActivity {
         }
 
         // запись пустой точки номер четыре ПРОБА
-        if (d.equals(abcdRef)){}
-        else {
+        if (!d.isEmpty()){
             //Получить текущую дату
             getTimNow();
 
             database04 = FirebaseDatabase.getInstance();
             ref04 = database04.getReference("Заявки")
-                    .child(MapOrder.getText().toString())
+                    .child(MapOrder)
                     .child(DataOrder.getText().toString())
                     .child(TimeOrder.getText().toString())
                     .child(RoutepOrder.getText().toString())
