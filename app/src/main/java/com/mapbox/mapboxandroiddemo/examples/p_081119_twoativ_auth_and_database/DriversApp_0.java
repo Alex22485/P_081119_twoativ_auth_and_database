95df46532c1d2db3c24a6e2209a3db8a7e4a533e
Alex22485 <alex.224.85@mail.ru>
2020-04-26 12:47:51 +0700

    модернизация  DriverApp_0 процесс регистрации в разработке

--- app/src/main/java/com/mapbox/mapboxandroiddemo/examples/p_081119_twoativ_auth_and_database/DriversApp_0.java
+++ app/src/main/java/com/mapbox/mapboxandroiddemo/examples/p_081119_twoativ_auth_and_database/DriversApp_0.java
@@ -26,7 +26,11 @@ import com.google.firebase.database.ValueEventListener;
 
 public class DriversApp_0 extends AppCompatActivity {
 
-    private static final String[] CARS= new String[]{"Alfa","Betta","Gamma",};
+    // Выпадающий массив для марок авто
+    private static final String[] CARS= new String[]{"Abarth","Acura","Alfa Romeo","Allard","Alpina","Alpine","Alvis","AMC","Ariel","Armstrong Siddeley","Ascari","Aston Martin","Audi","Austin","Austin-Healey","Autobianchi","Auverland","Avanti","Beijing","Bentley","Berkeley","Bitter","Bizzarrini","BMW","Brilliance","Bristol","Bugatti","Buick","BYD","Cadillac","Caterham","Changan","Checker","Chery","Chevrolet","Chrysler","Citroen","Dacia","Daewoo","DAF","Daihatsu","Daimler","Datsun","De Tomaso","DKW","Dodge","Dongfeng","Donkervoort","Eagle","Fairthorpe","FAW","Ferrari","Fiat","Ford","GAC","GAZ","Geely","Genesis","Ginetta","GMC","Great Wall","Haval","Holden","Honda","Hudson","Humber","Hummer","Hyundai","Infiniti","Innocenti","Isuzu","Italdesign","Iveco","Jaguar","Jeep","Jensen","Kia","Lada","Lamborghini","Lancia","Land Rover","Lexus","Lifan","Lincoln","Lotec","Lotus","Mahindra","Marcos","Marussia","Maserati","Matra-Simca","Maybach","Mazda","MCC","McLaren","Mercedes","Mercedes-Benz","Mercury","MG","Mini","Mitsubishi","Monteverdi","Moretti","Morgan","Morris","Nissan","Noble","NSU","Oldsmobile","Opel","Packard","Panoz","Peugeot","Pininfarina","Plymouth","Pontiac","Porsche","Proton","Ravon","Reliant","Renault","Riley","Rolls-Royce","Rover","Saab","Samsung","Saturn","Scion","Seat","Simca","Singer","Skoda","Smart","Spyker","Ssang Yong","SsangYong","Steyr","Studebaker","Subaru","Sunbeam","Suzuki","Talbot","Tata","Tatra","Tesla","Toyota","Trabant","Triumph","TVR","UAZ","Vauxhall","VAZ","Vector","Venturi","Volkswagen","Volvo","Wartburg","Westfield","Willys-Overland","Xedos","Zagato","Zastava","ZAZ","ZIL"};
+
+    // Выпадающий массив для цветовых оттенков машины
+    private static final String[] CARSCOLOR= new String[]{"баклажановый","бежевый","белый","желтый","зеленый","коричневый","красный","оранжевый","розовый","серый ","синий","фиолетовый","черный"};
 
     FirebaseAuth mAuth;
     String driverPhone;
@@ -42,8 +46,10 @@ public class DriversApp_0 extends AppCompatActivity {
     TextView TextHello1;
     TextView TextHello2;
 
+    EditText editDriverName;
     EditText editCar;
     EditText editNumberCar;
+    EditText editColorCar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,8 +59,11 @@ public class DriversApp_0 extends AppCompatActivity {
         checkWord=findViewById(R.id.checkWord);
         TextHello1=findViewById(R.id.TextHello1);
         TextHello2=findViewById(R.id.TextHello2);
+
+        editDriverName=findViewById(R.id.editDriverName);
         editCar=findViewById(R.id.editCar);
         editNumberCar=findViewById(R.id.editNumberCar);
+        editColorCar=findViewById(R.id.editColorCar);
 
 
 
@@ -101,7 +110,7 @@ public class DriversApp_0 extends AppCompatActivity {
                 String searchID=dataSnapshot.child(driverId).getValue(String.class);
                 checkWord.setText(searchID);
 
-                //Проверка есть ли уже регистрация
+                //Проверка есть ли уже регистрация!!!
                 visible();
                 }
                 @Override
@@ -120,7 +129,7 @@ public class DriversApp_0 extends AppCompatActivity {
     }
 
     public void visible(){
-
+        //ааа это название марки и три цифры гос номера
         String aaa=checkWord.getText().toString();
         if (!aaa.isEmpty()){
 
@@ -152,11 +161,18 @@ public class DriversApp_0 extends AppCompatActivity {
 
             editCar.setVisibility(View.VISIBLE);
             editNumberCar.setVisibility(View.VISIBLE);
+            editColorCar.setVisibility(View.VISIBLE);
+            editDriverName.setVisibility(View.VISIBLE);
 
+            // Выпадающий массив для марок авто
             AutoCompleteTextView editText=findViewById(R.id.editCar);
-            ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,
-                    android.R.layout.simple_list_item_1, CARS);
+            ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, CARS);
             editText.setAdapter(adapter);
+
+            // Выпадающий массив для цвета авто
+            AutoCompleteTextView editText1=findViewById(R.id.editColorCar);
+            ArrayAdapter<String> adapter1=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, CARSCOLOR);
+            editText1.setAdapter(adapter1);
 
         }
 
