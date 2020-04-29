c53fea6b925f3a3897370376c3a7f7bb52597784
Alex22485 <alex.224.85@mail.ru>
2020-04-29 22:19:07 +0700

    не рабочая версия

---
+++ app/src/main/java/com/mapbox/mapboxandroiddemo/examples/p_081119_twoativ_auth_and_database/DriversApp_Start.java
@@ -0,0 +1,14 @@
+package com.mapbox.mapboxandroiddemo.examples.p_081119_twoativ_auth_and_database;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DriversApp_Start extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drivers_app__start);
+    }
+}
