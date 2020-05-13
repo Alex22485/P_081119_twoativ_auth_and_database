4088f5e615676c4659781101d8e18111a455fe45
Alex22485 <alex.224.85@mail.ru>
2020-05-13 21:33:36 +0700

    проба инте соединения новая версия

--- app/src/main/java/com/mapbox/mapboxandroiddemo/examples/p_081119_twoativ_auth_and_database/MainActivity.java
+++ app/src/main/java/com/mapbox/mapboxandroiddemo/examples/p_081119_twoativ_auth_and_database/MainActivity.java
@@ -2,18 +2,11 @@ package com.mapbox.mapboxandroiddemo.examples.p_081119_twoativ_auth_and_database
 
 
 import androidx.annotation.NonNull;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-
 import android.os.Bundle;
-
 import android.os.Handler;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -27,12 +20,15 @@ import com.google.firebase.iid.InstanceIdResult;
 
     private static final String TAG ="MainActivity" ;
 
-    String  key;
+    String key;
     String keyReg;
     String UserToken;
 
     String internet;
-    String timeOut;
+    String internetTimeOut;
+
+    String registration;
+    String checkregistrationTimeOut;
 
     FirebaseDatabase database01;
     FirebaseDatabase database02;
@@ -51,7 +47,7 @@ import com.google.firebase.iid.InstanceIdResult;
             @Override
             public void onSuccess(InstanceIdResult instanceIdResult) {
                 UserToken = instanceIdResult.getToken();
-                Log.d(TAG, "токен"+UserToken);
+                Log.d(TAG, "токен: "+UserToken);
             }
         });
     }
@@ -60,7 +56,8 @@ import com.google.firebase.iid.InstanceIdResult;
     protected void onStart (){
         super.onStart();
         Log.d(TAG, "onStart");
-//Проверка интернета
+
+//СТАРТ Проверка интернета
         cheskInternet();
 // Проверка интернета
         //cheskInternet();
@@ -191,20 +188,22 @@ import com.google.firebase.iid.InstanceIdResult;
 
     //Проверка интернета
     public void cheskInternet(){
+
         key="";
         internet="";
-        timeOut="";
+        internetTimeOut="";
 
-                //задержка запроса
+        //ТАЙМ-АУТ ЗАПРОСА ИНТЕРНЕТА-1
         Handler handler1 = new Handler();
         handler1.postDelayed(new Runnable() {
             @Override
             public void run() {
-               //check();
+
+                // Завершен ТАЙМ-АУТ ЗАПРОСА ИНТЕРНЕТА-1
                inetNot();
-               timeOut="Out";
+               internetTimeOut="Out";
             }
-        },6000);
+        },7000);
 
 
         //чтение из БД с правилом для любых пользователей
@@ -217,14 +216,13 @@ import com.google.firebase.iid.InstanceIdResult;
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-
-                // Чтение
                 key=dataSnapshot.getValue(String.class);
-                internet="Yes";
-                cheskProba();
+                internet=key;
+                Log.d(TAG, "интернет 1 есть");
 
-                //временно для проверки
-                //Toast.makeText( MainActivity.this, "Интернет есть?  "+key, Toast.LENGTH_SHORT ).show();
+                //Проверка time-Out
+                timeOutInternet();
+
 
                 // с этой записью makeText появляется только один раз!!!!! ХОРОШО, блин не всегда :(((
                 ref01.removeEventListener(this);
@@ -235,52 +233,47 @@ import com.google.firebase.iid.InstanceIdResult;
         });
     }
 
-    public void check(){
-        // переход на Активити InternetNot
-        if (key.isEmpty()){
-            Intent aaa = new Intent(this,InternetNot.class);
-            startActivity(aaa);
-        }
-        else if(key.equals("Yes")) {
+        public void  inetNot(){
+            if (internet.equals("Yes")){
+                Log.d(TAG, "таймер-1 остановлен");/*специально пусто*/}
+            else {
+                Log.d(TAG, "Интернета нет при первом запросе");/*специально пусто*/
+                Intent aaa = new Intent(this,InternetNot.class);
+                startActivity(aaa);
+            }
+            }
 
-            //проверка регистрации
+        //Проверка time-Out
+        public void timeOutInternet (){
+        if(internetTimeOut.equals("Out")){
+            Log.d(TAG, "проверка интернета1 время вышло");/*специально пусто*/}
+
+        else {
+            // Проверка регистрации
             CheckRegistration();
         }
-    }
-
-        public void cheskProba(){
-
-        if(timeOut.equals("Out")){}
-        else {CheckRegistration();}
         }
-
-    public void  inetNot(){
-        if (internet.equals("Yes")){ }
-        else {
-            Intent aaa = new Intent(this,InternetNot.class);
-            startActivity(aaa);
-        }
-
-    }
 
     //проверка регистрации
     public void CheckRegistration(){
 
         keyReg="";
+        registration="";
+        checkregistrationTimeOut="";
 
-        //задержка запроса
+        //ТАЙМ-АУТ ЗАПРОСА ИНТЕРНЕТА-2
         Handler handler1 = new Handler();
         handler1.postDelayed(new Runnable() {
             @Override
             public void run() {
 
-                // Проверка регистрации токена
-                checkHaveToken();
-
+                // Завершен ТАЙМ-АУТ ЗАПРОСА ИНТЕРНЕТА-2 при проверке регистрации
+                checkregistrationTimeOut="Out";
+                inetNotWhenGoCheckRegistration();
             }
-        },600);
+        },7000);
 
-
+        Log.d(TAG, "запрос регистрации начат");
         //чтение из БД с правилом для любых пользователей
         database02 = FirebaseDatabase.getInstance();
         ref02 = database02.getReference("Check")
@@ -292,42 +285,55 @@ import com.google.firebase.iid.InstanceIdResult;
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-
-                // Чтение
                 keyReg=dataSnapshot.getValue(String.class);
-
-                //временно для проверки
-                //Toast.makeText( MainActivity.this, "Мой Токен найден?  "+keyReg, Toast.LENGTH_SHORT ).show();
+                registration=""+keyReg; /* так как может получить null*/
+                Log.d(TAG, "запрос регистрации получен");
 
                 // с этой записью makeText появляется только один раз!!!!! ХОРОШО
                 ref02.removeEventListener(this);
+
+                //Проверка токена
+                checkHaveToken();
             }
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
             }
         });
-
     }
 
-    //проверка зарегестрированного токена
+        //Проверка интернета во время проверки регистрации
+        public void inetNotWhenGoCheckRegistration (){
+
+            if(registration.equals("Hello")||keyReg==null){
+                Log.d(TAG, "таймер-2 остановлен");/*специально пусто*/}
+
+            else {
+                //пропал интернет во время проверки наличия регистрации
+                Log.d(TAG, "Интернета пропал при проверке регистрации");
+                Intent aaa = new Intent(this,InternetNot.class);
+                startActivity(aaa);
+            }
+        }
+
+    //Проверка токена
     public void checkHaveToken(){
-        // переход на Активити InternetNot
+
+        if (checkregistrationTimeOut.equals("Out")){
+            Log.d(TAG, "проверка интернета2 время вышло");/*специально пусто*/
+        }
+
+        else{
         if (keyReg==null){
 
             //переход к авторизации по телефону от firebase
             Intent AuthList = new Intent(this,Main2Activity.class);
             startActivity(AuthList);
         }
-        else if(keyReg.isEmpty()) {
-
-            //Если keyReg пусто, значит пропал интернет и переходим на  InternetNot
-            Intent aaa = new Intent(this,InternetNot.class);
-            startActivity(aaa);
-            //Toast.makeText( MainActivity.this, "опять нет интернета  "+keyReg, Toast.LENGTH_SHORT ).show();
-        }
         else if (keyReg.equals("Hello")){
             goMainList();}
     }
+    }
+
 
     //Переход в главное меню заказов
     public void goMainList(){
