bb2783d1b99261a534ef7dc7e6e267a44386ffe7
Alex22485 <alex.224.85@mail.ru>
2020-06-14 22:27:58 +0700

    начало новой полной версии

---
+++ app/src/main/java/com/mapbox/mapboxandroiddemo/examples/p_081119_twoativ_auth_and_database/MainUserNewOne.java
@@ -0,0 +1,14 @@
+package com.mapbox.mapboxandroiddemo.examples.p_081119_twoativ_auth_and_database;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainUserNewOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_user_new_one);
+    }
+}
