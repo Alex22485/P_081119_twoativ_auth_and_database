58c69a1c840059cfa8fb722da7fe5df98f383874
Alex22485 <alex.224.85@mail.ru>
2020-10-09 22:30:03 +0700

    Modern for SrartLists

---
+++ app/src/main/java/com/mapbox/mapboxandroiddemo/examples/p_081119_twoativ_auth_and_database/ViewMapKrasnoyarsk.java
@@ -0,0 +1,25 @@
+package com.mapbox.mapboxandroiddemo.examples.p_081119_twoativ_auth_and_database;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class ViewMapKrasnoyarsk extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_map_krasnoyarsk);
+    }
+    public void goBack(View view){
+        Intent goBack = new Intent (this,MainUserNewOne2.class);
+        startActivity(goBack);
+    }
+
+    public void goToOder (View view){
+        Intent goToOder = new Intent(this,MainUserNewOne3.class);
+        startActivity(goToOder);
+    }
+}
