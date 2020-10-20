0a8b9e4ca2d3e89bdafef2caeec928237298ab06
Alex22485 <alex.224.85@mail.ru>
2020-10-20 19:17:43 +0700

    изучал немного фрагменты

--- app/src/main/java/com/mapbox/mapboxandroiddemo/examples/p_081119_twoativ_auth_and_database/Zakaz1.java
+++ app/src/main/java/com/mapbox/mapboxandroiddemo/examples/p_081119_twoativ_auth_and_database/Zakaz1.java
@@ -1,23 +1,182 @@
 package com.mapbox.mapboxandroiddemo.examples.p_081119_twoativ_auth_and_database;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Zakaz1 extends AppCompatActivity {
+
+    private static final String TAG ="Zakaz1" ;
+
+    Button button1,button2,button3,button4;
+    TextView Right1,Right2,Right3,Right4,Right5;
+    TextView No1,No2,No3,No4,No5;
+    TextView OderRight;
+
+    String [] listCityTaxi= {"Красноярск","Сосновоборск","Ачинск","Канск","Лесосибирск"};
+    String refCityTaxi,refFromInCity;
+    String [] listCityFromIn= {};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zakaz1);
+
+        button1=findViewById(R.id.button1);
+        button2=findViewById(R.id.button2);
+        button3=findViewById(R.id.button3);
+        button4=findViewById(R.id.button4);
+        Right1=findViewById(R.id.Right1);
+        Right2=findViewById(R.id.Right2);
+        Right3=findViewById(R.id.Right3);
+        Right4=findViewById(R.id.Right4);
+        Right5=findViewById(R.id.Right5);
+        No1=findViewById(R.id.No1);
+        No2=findViewById(R.id.No2);
+        No3=findViewById(R.id.No3);
+        No4=findViewById(R.id.No4);
+        No5=findViewById(R.id.No5);
+        OderRight=findViewById(R.id.OderRight);
+
+        button2.setVisibility(View.INVISIBLE);
+        button3.setVisibility(View.INVISIBLE);
+        button4.setVisibility(View.INVISIBLE);
+        Right1.setVisibility(View.INVISIBLE);
+        Right2.setVisibility(View.INVISIBLE);
+        Right3.setVisibility(View.INVISIBLE);
+        Right4.setVisibility(View.INVISIBLE);
+        Right5.setVisibility(View.INVISIBLE);
+        No2.setVisibility(View.INVISIBLE);
+        No3.setVisibility(View.INVISIBLE);
+        No4.setVisibility(View.INVISIBLE);
+        No5.setVisibility(View.INVISIBLE);
+        OderRight.setVisibility(View.INVISIBLE);
+
+        dinamicView();
+
     }
 
-public void toGo(View view){
-        FragmentZakaz1 ft=new FragmentZakaz1();
-    FragmentTransaction fr= getSupportFragmentManager().beginTransaction();
-    fr.replace(R.id.container,ft);
-    fr.commit();
-}
+    // кнопка button1
+    public void button1(View view){
+        AlertChoisCity();
+    }
+    // кнопка button2
+    public void button2(View view){
+
+    }
+    // кнопка button3
+    public void button3(View view){
+
+    }
+    // кнопка button4
+    public void button4(View view){
+
+    }
+
+
+    public void dinamicView(){
+
+        Handler handler1 = new Handler();
+        handler1.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                button2.setVisibility(View.VISIBLE);
+                No2.setVisibility(View.VISIBLE);
+            }
+        },300);
+
+        Handler handler2 = new Handler();
+        handler2.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                button3.setVisibility(View.VISIBLE);
+                No3.setVisibility(View.VISIBLE);
+            }
+        },600);
+
+        Handler handler3 = new Handler();
+        handler3.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                button4.setVisibility(View.VISIBLE);
+                No4.setVisibility(View.VISIBLE);
+            }
+        },900);
+
+        Handler handler4 = new Handler();
+        handler4.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                OderRight.setVisibility(View.VISIBLE);
+                No5.setVisibility(View.VISIBLE);
+
+                //выбор города
+                AlertChoisCity();
+                button1.setEnabled(true);
+            }
+        },1200);
+
+    }
+    public void AlertChoisCity(){
+
+
+        AlertDialog.Builder mAlertDialog = new AlertDialog.Builder(Zakaz1.this);
+        mAlertDialog.setTitle("Выбери город");
+        mAlertDialog
+                .setItems(listCityTaxi, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        refCityTaxi=listCityTaxi[which];
+
+                        //выбор в/из города
+                        AlertFromInCity();
+                    }
+                });
+        mAlertDialog.create();
+        mAlertDialog.show();
+    }
+
+    public void AlertFromInCity(){
+
+        listCityFromIn= new String[]{refCityTaxi + "->Аэропорт","Аэропорт->"+refCityTaxi};
+
+        AlertDialog.Builder mAlertDialog = new AlertDialog.Builder(Zakaz1.this);
+        mAlertDialog.setTitle("Выбери город");
+        mAlertDialog
+                .setItems(listCityFromIn, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        refFromInCity=listCityFromIn[which];
+
+                        No1.setVisibility(View.INVISIBLE);
+                        Right1.setVisibility(View.VISIBLE);
+                        button2.setEnabled(true);
+
+                        //выбор маршрута
+                        Zakaz1ToZakaz2();
+                    }
+                });
+        mAlertDialog.create();
+        mAlertDialog.show();
+    }
+
+    public void Zakaz1ToZakaz2(){
+        Intent Zakaz1ToZakaz2=new Intent(this,Zakaz2.class);
+        Zakaz1ToZakaz2.putExtra("refFromInCity",refFromInCity);
+        startActivity(Zakaz1ToZakaz2);
+
+
+
+    }
+
 }
---
+++ app/src/main/java/com/mapbox/mapboxandroiddemo/examples/p_081119_twoativ_auth_and_database/Zakaz2.java
@@ -0,0 +1,284 @@
+package com.mapbox.mapboxandroiddemo.examples.p_081119_twoativ_auth_and_database;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Zakaz2 extends AppCompatActivity {
+
+    TextView textView;
+    Button button1,button2,button3,button4,button5;
+    Button map1,map2,map3,map4,map5;
+    String refFromInCity;
+    String[] array1 ={"Щорса-Аэропорт","КрасТэц-Аэропорт","Северный-Аэропорт","ЖД вокзал-Аэропорт","Ветлужанка-Аэропорт"};
+    String[] array2 ={"Аэропорт-Щорса","Аэропорт-КрасТэц","Аэропорт-Северный","Аэропорт-ЖД вокзал","Аэропорт-Ветлужанка",};
+
+    // пункты сбора Красноярск
+    String[] KrasnojarskOneMap={"Кинотеатр Металлург","Автобусный пер","Пикра","Мебельная фабрика"};
+    String[] KrasnojarskTwoMap = {"ДК КрасТЭЦ","Аэрокосмическая академия","Торговый центр","Предмостная пл"};
+    String[] KrasnojarskTreeMap = {"xxx","xxx","xxx","xxx"};
+    String[] KrasnojarskFourMap = {"yyy","yyy","yyy","yyy"};
+    String[] KrasnojarskFiveMap = {"uuu","uuu","Тuuu","uuu"};
+
+    // пункты сбора других городов
+    String[] AnyCity={"Подтвердить?"};
+
+    // пункт сбора из Аэропорта
+    String[] FromAirport={"Парковка Р2"};
+
+    String[] RefList1;
+    String[] RefList2;
+    String[] RefList3;
+    String[] RefList4;
+    String[] RefList5;
+
+    String RefPoint;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zakaz2);
+
+        textView=findViewById(R.id.textView);
+        button1=findViewById(R.id.button1);
+        button2=findViewById(R.id.button2);
+        button3=findViewById(R.id.button3);
+        button4=findViewById(R.id.button4);
+        button5=findViewById(R.id.button5);
+        map1=findViewById(R.id.map1);
+        map2=findViewById(R.id.map2);
+        map3=findViewById(R.id.map3);
+        map4=findViewById(R.id.map4);
+        map5=findViewById(R.id.map5);
+
+        button1.setVisibility(View.INVISIBLE);
+        button2.setVisibility(View.INVISIBLE);
+        button3.setVisibility(View.INVISIBLE);
+        button4.setVisibility(View.INVISIBLE);
+        button5.setVisibility(View.INVISIBLE);
+        map1.setVisibility(View.INVISIBLE);
+        map2.setVisibility(View.INVISIBLE);
+        map3.setVisibility(View.INVISIBLE);
+        map4.setVisibility(View.INVISIBLE);
+        map5.setVisibility(View.INVISIBLE);
+
+        Intent Zakaz1ToZakaz2=getIntent();
+        refFromInCity= Zakaz1ToZakaz2.getStringExtra("refFromInCity");
+
+        visalList();
+    }
+
+    public void visalList(){
+        if(refFromInCity.equals("Красноярск->Аэропорт")){
+
+            button1.setVisibility(View.VISIBLE);
+            button2.setVisibility(View.VISIBLE);
+            button3.setVisibility(View.VISIBLE);
+            button4.setVisibility(View.VISIBLE);
+            button5.setVisibility(View.VISIBLE);
+            map1.setVisibility(View.VISIBLE);
+            map2.setVisibility(View.VISIBLE);
+            map3.setVisibility(View.VISIBLE);
+            map4.setVisibility(View.VISIBLE);
+            map5.setVisibility(View.VISIBLE);
+
+            button1.setText(array1[0]);
+            button2.setText(array1[1]);
+            button3.setText(array1[2]);
+            button4.setText(array1[3]);
+            button5.setText(array1[4]);
+
+            RefList1=KrasnojarskOneMap;
+            RefList2=KrasnojarskTwoMap;
+            RefList3=KrasnojarskTreeMap;
+            RefList4=KrasnojarskFourMap;
+            RefList5=KrasnojarskFiveMap;
+
+
+
+    }
+
+        if(refFromInCity.equals("Аэропорт->Красноярск")){
+
+            button1.setVisibility(View.VISIBLE);
+            button2.setVisibility(View.VISIBLE);
+            button3.setVisibility(View.VISIBLE);
+            button4.setVisibility(View.VISIBLE);
+            button5.setVisibility(View.VISIBLE);
+            map1.setVisibility(View.VISIBLE);
+            map2.setVisibility(View.VISIBLE);
+            map3.setVisibility(View.VISIBLE);
+            map4.setVisibility(View.VISIBLE);
+            map5.setVisibility(View.VISIBLE);
+
+            button1.setText(array2[0]);
+            button2.setText(array2[1]);
+            button3.setText(array2[2]);
+            button4.setText(array2[3]);
+            button5.setText(array2[4]);
+
+            RefList1=FromAirport;
+            RefList2=FromAirport;
+            RefList3=FromAirport;
+            RefList4=FromAirport;
+            RefList5=FromAirport;
+
+
+        }
+
+        if(refFromInCity.equals("Сосновоборск->Аэропорт")){
+
+            button1.setVisibility(View.VISIBLE);
+            map1.setVisibility(View.VISIBLE);
+            RefList1=AnyCity;
+            button1.setText(refFromInCity);
+
+        }
+
+        if(refFromInCity.equals("Аэропорт->Сосновоборск")){
+            button1.setVisibility(View.VISIBLE);
+            map1.setVisibility(View.VISIBLE);
+            button1.setText(refFromInCity);
+            RefList1=FromAirport;
+        }
+
+        if(refFromInCity.equals("Ачинск->Аэропорт")){
+            button1.setVisibility(View.VISIBLE);
+            map1.setVisibility(View.VISIBLE);
+            button1.setText(refFromInCity);
+            RefList1=AnyCity;
+        }
+
+        if(refFromInCity.equals("Аэропорт->Ачинск")){
+            button1.setVisibility(View.VISIBLE);
+            map1.setVisibility(View.VISIBLE);
+            button1.setText(refFromInCity);
+            RefList1=FromAirport;
+        }
+
+        if(refFromInCity.equals("Канск->Аэропорт")){
+            button1.setVisibility(View.VISIBLE);
+            map1.setVisibility(View.VISIBLE);
+            button1.setText(refFromInCity);
+            RefList1=AnyCity;
+        }
+
+        if(refFromInCity.equals("Аэропорт->Канск")){
+            button1.setVisibility(View.VISIBLE);
+            map1.setVisibility(View.VISIBLE);
+            button1.setText(refFromInCity);
+            RefList1=FromAirport;
+        }
+
+        if(refFromInCity.equals("Лесосибирск->Аэропорт")){
+            button1.setVisibility(View.VISIBLE);
+            map1.setVisibility(View.VISIBLE);
+            button1.setText(refFromInCity);
+            RefList1=AnyCity;
+        }
+
+        if(refFromInCity.equals("Аэропорт->Лесосибирск")){
+            button1.setVisibility(View.VISIBLE);
+            map1.setVisibility(View.VISIBLE);
+            button1.setText(refFromInCity);
+            RefList1=FromAirport;
+        }
+    }
+    public void button1 (View view){
+
+        AlertDialog.Builder mAlertDialog = new AlertDialog.Builder(Zakaz2.this);
+        mAlertDialog.setTitle("Пункт сбора");
+        mAlertDialog
+                .setItems(RefList1, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        RefPoint=RefList1[which];
+
+                    }
+                });
+        mAlertDialog.create();
+        mAlertDialog.show();
+
+    }
+
+    public void button2 (View view){
+
+        AlertDialog.Builder mAlertDialog = new AlertDialog.Builder(Zakaz2.this);
+        mAlertDialog.setTitle("Пункт сбора");
+        mAlertDialog
+                .setItems(RefList2, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        RefPoint=RefList2[which];
+
+                    }
+                });
+        mAlertDialog.create();
+        mAlertDialog.show();
+
+    }
+
+    public void button3 (View view){
+
+        AlertDialog.Builder mAlertDialog = new AlertDialog.Builder(Zakaz2.this);
+        mAlertDialog.setTitle("Пункт сбора");
+        mAlertDialog
+                .setItems(RefList3, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        RefPoint=RefList3[which];
+
+                    }
+                });
+        mAlertDialog.create();
+        mAlertDialog.show();
+
+    }
+
+    public void button4 (View view){
+
+        AlertDialog.Builder mAlertDialog = new AlertDialog.Builder(Zakaz2.this);
+        mAlertDialog.setTitle("Пункт сбора");
+        mAlertDialog
+                .setItems(RefList4, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        RefPoint=RefList4[which];
+
+                    }
+                });
+        mAlertDialog.create();
+        mAlertDialog.show();
+
+    }
+
+    public void button5 (View view){
+
+        AlertDialog.Builder mAlertDialog = new AlertDialog.Builder(Zakaz2.this);
+        mAlertDialog.setTitle("Пункт сбора");
+        mAlertDialog
+                .setItems(RefList5, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        RefPoint=RefList5[which];
+
+                    }
+                });
+        mAlertDialog.create();
+        mAlertDialog.show();
+
+    }
+}
