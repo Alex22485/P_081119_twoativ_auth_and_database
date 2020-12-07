86ee87f62c7631a41e643e14f39033b12d6106b4
Alex22485 <alex.224.85@mail.ru>
2020-12-07 19:39:09 +0700

    1.09.2020 новые изменения регистрации без  задержек в на первом листе (длительная задержка 20сек только один раз  при регистрации телефона)

--- app/src/main/java/com/mapbox/mapboxandroiddemo/examples/p_081119_twoativ_auth_and_database/MainActivity.java
+++ app/src/main/java/com/mapbox/mapboxandroiddemo/examples/p_081119_twoativ_auth_and_database/MainActivity.java
@@ -8,22 +8,13 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.InstanceIdResult;
-import com.google.firebase.installations.FirebaseInstallations;
 import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
