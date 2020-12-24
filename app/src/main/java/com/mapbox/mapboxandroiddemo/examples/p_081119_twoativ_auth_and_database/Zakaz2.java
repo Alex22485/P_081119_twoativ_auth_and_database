c0d4fd87c3ca0353315fbf4d50ad9a85597d5166
Alex22485 <alex.224.85@mail.ru>
2020-12-24 13:10:45 +0700

    21.12.2020 Рабочий код включая сервер+водитель

--- app/src/main/java/com/mapbox/mapboxandroiddemo/examples/p_081119_twoativ_auth_and_database/Zakaz2.java
+++ app/src/main/java/com/mapbox/mapboxandroiddemo/examples/p_081119_twoativ_auth_and_database/Zakaz2.java
@@ -312,9 +312,14 @@ public class Zakaz2 extends AppCompatActivity {
         backZakaz2ToZakaz1.putExtra("phoneNew",phoneNew);
         startActivity(backZakaz2ToZakaz1);
     }
-    // Открытие карты маршрута
+    // Открытие АЭРОПОРТ-ЩОРСА
     public void Map1(View view){
         Intent Map1 =new Intent(this,Map1.class);
+        startActivity(Map1);
+    }
+    // Открытие АЭРОПОРТ-КРАСТЭЦ
+    public void Map2(View view){
+        Intent Map1 =new Intent(this,Map2_Air_KrasTeth.class);
         startActivity(Map1);
     }
 
